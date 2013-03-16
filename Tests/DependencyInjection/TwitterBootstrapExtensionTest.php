134d116e5752812960ab468405287dc936a0353b
andrey <asamusev@archer-soft.com>
2013-03-16 11:58:54 +0200

    add DependencyInjection test

---
+++ Tests/DependencyInjection/TwitterBootstrapExtensionTest.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * @author Andrey <asamusev@archer-soft.com>
+ * @copyright andrey 3/16/13
+ * @version 1.0.0
+ */
+
+namespace Twitter\BootstrapBundle\Tests\DependencyInjection;
+
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBag;
+use Twitter\BootstrapBundle\DependencyInjection\TwitterBootstrapExtension;
+use Twitter\BootstrapBundle\Tests\TestCase;
+
+class TwitterBootstrapExtensionTest extends TestCase
+{
+    public function testLoadEmptyConfiguration()
+    {
+        $container = $this->createContainer();
+        $container->registerExtension(new TwitterBootstrapExtension());
+        $container->loadFromExtension('twitter_bootstrap', array());
+        $this->compileContainer($container);
+    }
+
+    private function createContainer()
+    {
+        $container = new ContainerBuilder(new ParameterBag(array(
+            'kernel.cache_dir' => __DIR__,
+            'kernel.root_dir' => __DIR__ . '/Fixtures',
+            'kernel.charset' => 'UTF-8',
+            'kernel.debug' => false,
+            'kernel.bundles' => array('TwitterBootstrapBundle' => 'Twitter\\BootstrapBundle\\TwitterBootstrapBundle'),
+        )));
+
+        return $container;
+    }
+
+    private function compileContainer(ContainerBuilder $container)
+    {
+        $container->getCompilerPassConfig()->setOptimizationPasses(array());
+        $container->getCompilerPassConfig()->setRemovingPasses(array());
+        $container->compile();
+    }
+
+}
---
+++ Tests/TestCase.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @author Andrey <asamusev@archer-soft.com>
+ * @copyright andrey 3/16/13
+ * @version 1.0.0
+ */
+
+namespace Twitter\BootstrapBundle\Tests;
+
+
+abstract class TestCase  extends \PHPUnit_Framework_TestCase
+{
+    protected function setUp()
+    {
+        if (!class_exists('Twig_Environment')) {
+            $this->markTestSkipped('Twig is not available.');
+        }
+    }
+}
