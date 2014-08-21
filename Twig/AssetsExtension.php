565a05647ac285dea5e4c2b24bb2ea28b8c349ae
Andrey Samusev <a.samusev@owox.com>
2014-08-21 15:33:38 +0400

    add Twig Ext

---
+++ Twig/AssetsExtension.php
@@ -0,0 +1,77 @@
+<?php
+namespace Twitter\BootstrapBundle\Twig;
+
+use Symfony\Bundle\TwigBundle\Extension\AssetsExtension as BaseExtension;
+
+class AssetsExtension extends \Twig_Extension
+{
+    /** @var array */
+    private $assets = [];
+    /** @var \Symfony\Bundle\TwigBundle\Extension\AssetsExtension */
+    private $extension;
+    /** @var array */
+    private $pattern = [
+        'stylesheets' => '<link href="{{ value }}" rel="stylesheet" type="text/css"/>',
+        'javascripts' => '<script src="{{ value }}" type="text/javascript"></script>'
+    ];
+
+    /**
+     * @param BaseExtension $extension
+     * @param array         $assets
+     * @param array         $pattern
+     */
+    public function __construct(BaseExtension $extension, array $assets = array(), array $pattern = array())
+    {
+        $this->extension = $extension;
+        $this->assets = $assets;
+        $this->pattern = array_merge($this->pattern, $pattern);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getFunctions()
+    {
+        return array(
+            new \Twig_SimpleFunction(
+                'twitter_assets', array($this, 'assetsFunction'), array('is_safe' => array('html'))
+            ),
+        );
+    }
+
+    /**
+     * Returns the public path of an asset in the pattens.
+     *
+     * Absolute paths (i.e. http://...) are returned unmodified.
+     *
+     * @param string           $type        A type assets
+     * @param string           $packageName The name of the asset package to use
+     * @param bool             $absolute    Whether to return an absolute URL or a relative one
+     * @param string|bool|null $version     A specific version
+     *
+     * @return string
+     */
+    public function assetsFunction($type = 'stylesheets', $packageName = null, $absolute = false, $version = null)
+    {
+        $string = '';
+        $data = isset($this->assets[$type]) ? $this->assets[$type] : array();
+        if (isset($this->pattern[$type])) {
+            foreach ($data as $value) {
+                $url = $this->extension->getAssetUrl($value, $packageName, $absolute, $version);
+                $string .= str_replace('{{ value }}', $url, $this->pattern[$type]);
+            }
+        }
+
+        return $string;
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getName()
+    {
+        return 'twitter_assets';
+    }
+
+}
